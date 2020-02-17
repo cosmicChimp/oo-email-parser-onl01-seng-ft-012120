66e74c703c0b40a86173ffc13818cb8e05b9aa67
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 17:03:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,4 @@
-class EmailParser
+class EmailAddressParser
 attr_accessor :emails 
 
 def initialize(emails)
@@ -14,9 +14,9 @@ end
 end
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
+parser1 = EmailAddressParser.new(emails1)
 parser1.parse 
 
 emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
+parser2 = EmailAddressParser.new(emails2)
 parser2.parse
