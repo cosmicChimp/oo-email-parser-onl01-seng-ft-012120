ee49f6a863217f405a07a71a6f22998f4300b701
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-17 17:07:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,10 +13,3 @@ end
 
 end
 
-# emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-# parser1 = EmailAddressParser.new(emails1)
-# parser1.parse 
-
-# emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-# parser2 = EmailAddressParser.new(emails2)
-# parser2.parse
